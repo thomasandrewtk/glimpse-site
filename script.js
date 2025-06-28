1567e536f557de87dffbea718f599bc1832a07b7
thomasandrewtk <tandrewtk@gmail.com>
2025-06-27 19:19:21 -0500

    feat: Add page animations and update mobile alignment

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // You can add more scripts here if needed.
     console.log('Glimpse site loaded');
     
+    document.body.classList.add('loaded');
+    
     // Add any analytics or tracking code here if needed
 }); 
